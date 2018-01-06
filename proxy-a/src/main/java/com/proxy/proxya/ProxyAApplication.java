c5e43f6fdf243fe07e460d41d83b81d1646c8df6
kobet <weigeshikebi@163.com>
2018-01-06 16:08:34 +0800

    eureka-server init

---
+++ proxy-a/src/main/java/com/proxy/proxya/ProxyAApplication.java
@@ -0,0 +1,12 @@
+package com.proxy.proxya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyAApplication.class, args);
+	}
+}
---
+++ proxy-b/src/main/java/com/proxy/proxyb/ProxyBApplication.java
@@ -0,0 +1,12 @@
+package com.proxy.proxyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyBApplication.class, args);
+	}
+}
