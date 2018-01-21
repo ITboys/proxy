f6b249be3dd0e32d0c8fc34f9511ad610d807033
kobet <weigeshikebi@163.com>
2018-01-21 10:46:16 +0800

    add pay

--- proxy-a/src/main/resources/static/js/pay.js
+++ proxy-a/src/main/resources/static/js/pay.js
@@ -9,10 +9,11 @@ $(function() {
 
 	var price = 1; // 表单提交的价格
 	var type = 1; // 支付类型 1：支付宝 2：QQ钱包 3：微信
-	var payId = 1111; // 支付人的唯一标识
+	var payId = Math.round(Math.random() * 10000); // 支付人的唯一标识
+	alert( "=================" + payId);
 	var param = ""; // 自定义一些参数 支付后返回
 
-	var notify_url = "http://3col.cn/pay/notice";// 通知地址
+	//var notify_url = "http://3col.cn/pay/notice";// 通知地址、
 	var return_url = "http://3col.cn/pay/notice";// 支付后同步跳转地址
 
 	if (price == null) {
@@ -22,8 +23,9 @@ $(function() {
 	// 参数有中文则需要URL编码
 	var url = "http://codepay.fateqq.com:52888/creat_order?id=" + codepay_id
 			+ "&pay_id=" + payId + "&price=" + price + "&type=" + type
-			+ "&token=" + token + "&param=" + param + "&notify_url="
-			+ notify_url + "&return_url=" + return_url;
+			+ "&token=" + token + "&param=" + param 
+//			+ "&notify_url=" + notify_url 
+			+ "&return_url=" + return_url;
 
 	window.location.href = url;
 
